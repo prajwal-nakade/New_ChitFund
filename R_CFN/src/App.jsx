f6bc4ed334be5b17a0744a0fbd475c5d799c462d
prajwalnakade <prajwalnakade478@gmail.com>
2026-03-09 10:57:26 +0530

    added more authentication forms

--- R_CFN/src/App.jsx
+++ R_CFN/src/App.jsx
@@ -2,8 +2,8 @@ import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router";
 import Sidebar from "./components/sidebar";
 import ApplcationForm from "./pages/aplication_form";
-  import { ToastContainer, toast } from 'react-toastify';
-  import 'react-toastify/ReactToastify.css'
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/ReactToastify.css";
 import LoginPage from "./pages/LoginPage";
 import SignupPage from "./pages/SignupPage";
 import CustomerApplication from "./pages/CustomerApplication";
@@ -13,28 +13,50 @@ import ChitAgreement from "./pages/ChitAgreement";
 import AgreementPrintPreview from "./pages/AgreementPrintPreview";
 import ViewChitAgreement from "./pages/ViewChitAgreement";
 import AuthenticationForm from "./pages/AuthenticationForm";
-import MeetingMinutesForm from "./pages/MeetingMinutesForm";
+import ViewAuthenticationForm from "./pages/ViewAuthenticationForm";
+import PromissoryNote from "./pages/PromissoryNote";
+import DemandPromissoryNote from "./pages/DemandPromissoryNote";
+import GuaranteeAgreement from "./pages/GuaranteeAgreement";
 
 function App() {
-
   return (
     <>
-    <Router>
-      <Routes>
-        <Route path="/application-form" element={<ApplcationForm />} />
-        <Route path="/" element={<LoginPage />} />
-        <Route path="/register" element={<SignupPage />}/>
-        <Route path="/customerapplication" element={<CustomerApplication />} />
-        <Route path="/chit/print/:id" element={<ChitPrintPreview />} />
-        <Route path="/viewapplications" element={<ApplicationViewMaster />} />
-        <Route path="/ChitAgreement" element={<ChitAgreement />} />
-        <Route path="/AgreementPrintPreview/:id" element={<AgreementPrintPreview />} />
-        <Route path="/ViewChitAgreement" element={<ViewChitAgreement />} />
-        <Route path="/authenticationform" element={<AuthenticationForm/>} />
-        <Route path="/MeetingMinutesForm" element={<MeetingMinutesForm/>} />
-      </Routes>
-    </Router>
-    <ToastContainer />
+      <Router>
+        <Routes>
+          <Route path="/application-form" element={<ApplcationForm />} />
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/register" element={<SignupPage />} />
+          <Route
+            path="/customerapplication"
+            element={<CustomerApplication />}
+          />
+          <Route path="/chit/print/:id" element={<ChitPrintPreview />} />
+          <Route path="/viewapplications" element={<ApplicationViewMaster />} />
+          <Route path="/ChitAgreement" element={<ChitAgreement />} />
+          <Route
+            path="/AgreementPrintPreview/:id"
+            element={<AgreementPrintPreview />}
+          />
+          <Route path="/ViewChitAgreement" element={<ViewChitAgreement />} />
+          <Route
+            path="/authenticationform/:id"
+            element={<AuthenticationForm />}
+          />
+          <Route
+            path="/viewauthenticationform"
+            element={<ViewAuthenticationForm />}
+          />
+          <Route path="/promissorynote" element={<PromissoryNote />} />
+
+          <Route
+            path="/DemandPromiissoryNote"
+            element={<DemandPromissoryNote />}
+          />
+
+          <Route path="/GuaranteeAgreement" element={<GuaranteeAgreement />} />
+        </Routes>
+      </Router>
+      <ToastContainer />
     </>
   );
 }
---
+++ R_CFN/src/pages/PromissoryNote.jsx
@@ -0,0 +1,134 @@
+import React from "react";
+
+const PromissoryNote = () => {
+  return (
+    <div className="max-w-4xl mx-auto bg-white border border-black px-8 py-6 text-[16px] leading-6 ">
+
+      {/* TOP RIGHT CIN */}
+      <div className="text-end text-xs">CIN NO.U64990MH2023PTC400938</div>
+
+        {/* HEADER */}
+        <div className="flex items-center mt-2">
+          <img src="/Logo2.png" alt="" className="w-20" />
+
+          <div className="mx-auto text-center w-full flex flex-col items-center">
+            <img src="/Logo.png" alt="" className="w-72" />
+            <p className="text-xs mt-1">
+              Plot No.7, Gut No.216, Satara Road, Near Ahilyabai Holkar Chauk,
+              Satara Parisar, Chh. Sambhajinagar.
+            </p>
+          </div>
+        </div>
+
+      {/* TITLE BAR */}
+      <div className="bg-black text-white text-center text-[13px] font-bold py-[3px] mt-2">
+        PROMISSORY NOTE
+      </div>
+
+      {/* DATE */}
+      <div className="flex justify-end mt-3">
+        <span className="mr-2">Date :</span>
+        <input className="border-b border-black w-48 outline-none bg-transparent" />
+      </div>
+
+      {/* REF + AMOUNT */}
+      <div className="mt-3">
+        <div className="flex items-center">
+          <span className="mr-2">CHIT REF :</span>
+          <input className="border-b border-black w-64 outline-none bg-transparent" />
+        </div>
+
+        <div className="flex items-center mt-1">
+          <span className="mr-2">Amount Rs:</span>
+          <input className="border-b border-black w-40 outline-none bg-transparent" />
+        </div>
+      </div>
+
+      {/* BODY TEXT */}
+      <div className="mt-4 text-justify">
+        ON DEMAND I/WE Jointly and Severally do hereby promise to pay KARDE
+        KRISHNA CHITS PRIVATE LTD. Of Order the Sum of ( Rupees
+        <span className="inline-block border-b border-black w-130 mx-2"></span>
+        only )
+      </div>
+
+      <div className="mt-3">
+        Repayable with interest at
+        <span className="inline-block border-b border-black w-20 mx-2"></span>
+        Percent per annum being the amount of the future installment due to
+        Messrs.
+      </div>
+
+      {/* COMPANY SECTION */}
+      <div className="mt-4">
+        <div className="font-bold">
+          KARDE KRISHNA CHITS PRIVATE LIMITED
+        </div>
+
+        <div className="mt-2">
+          In respect of Ticket No.
+          <span className="inline-block border-b border-black w-20 mx-2"></span>
+          in
+          <span className="inline-block border-b border-black w-32 mx-2"></span>
+          series, in Monthly Installment of Rs.
+          <span className="inline-block border-b border-black w-24 mx-2"></span>
+        </div>
+
+        <div className="mt-1">
+          ( Rupees
+          <span className="inline-block border-b border-black w-64 mx-2"></span>
+          only ) for
+          <span className="inline-block border-b border-black w-16 mx-2"></span>
+          Months
+          <span className="inline-block border-b border-black w-64 mx-2"></span>
+        </div>
+      </div>
+
+      {/* SIGNATURE AREA */}
+      <div className="mt-10 space-y-15">
+
+        {/* Subscriber */}
+        <div className="flex justify-between items-end">
+          <div className="w-[400px] flex flex-col items-center justify-center mx-auto">
+            <div className="border-b border-black w-full"></div>
+            <p className="text-sm mt-1 text-center">Name Of Subscriber</p>
+          </div>
+
+          <div className="grid grid-cols-2 gap-15 mr-10">
+            <div className="scale-150 w-14 h-16 border border-black"></div>
+            <div className="w-14 h-16 border border-black"></div>
+          </div>
+        </div>
+
+        {/* Guarantor 01 */}
+        <div className="flex justify-between items-end">
+          <div className="w-[400px] flex flex-col items-center justify-center mx-auto">
+            <div className="border-b border-black w-full"></div>
+            <p className="text-sm mt-1 text-center">Name Of Guarantor 01</p>
+          </div>
+
+          <div className="grid grid-cols-2 gap-15 mr-10">
+            <div className="scale-150 w-14 h-16 border border-black"></div>
+            <div className="w-14 h-16 border border-black"></div>
+          </div>
+        </div>
+
+        {/* Guarantor 02 */}
+        <div className="flex justify-between items-end ">
+          <div className="w-[400px] flex flex-col items-center justify-center mx-auto">
+            <div className="border-b border-black w-full"></div>
+            <p className="text-sm text-center mt-1">Name Of Guarantor 02</p>
+          </div>
+
+          <div className="grid grid-cols-2 gap-15 mr-10">
+            <div className=" scale-150 w-14 h-16 border border-black"></div>
+            <div className="w-14 h-16 border border-black"></div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  );
+};
+
+export default PromissoryNote;
