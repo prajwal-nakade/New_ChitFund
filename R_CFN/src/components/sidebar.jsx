9ee2e340b55e9544b71373a598726148770d3068
Amrut Deshpande <amrutdeshpande2004@gmail.com>
2026-03-20 11:10:12 +0530

    Extra api calls

--- R_CFN/src/components/sidebar.jsx
+++ R_CFN/src/components/sidebar.jsx
@@ -7,18 +7,19 @@ const Sidebar = () => {
 
   const Menus = [
     { title: "Application Form", icon: <ClipboardList size={16} />, path: "/customerapplication" },
-    { title: "View Applications", icon : <Table2 size={16}/>, path: "/viewapplications" },
-    { title: "Chit Agreement", icon : <Handshake size={16}/>, path: "/ChitAgreement" },
-    
-    { title: "View Chit Agreement", icon : <ClipboardList size={16}/>, path: "/ViewChitAgreement" },
+    { title: "View Applications", icon: <Table2 size={16} />, path: "/viewapplications" },
+    { title: "Chit Agreement", icon: <Handshake size={16} />, path: "/ChitAgreement" },
+
+    { title: "View Chit Agreement", icon: <ClipboardList size={16} />, path: "/ViewChitAgreement" },
+    { title: "Authentication Form", icon: <ClipboardList size={16} />, path: "/viewauthenticationform" },
     // { title: "Menu 2", icon: <LayoutDashboard size={16} />, path: "/menu2" },
   ];
 
   const masterSubMenus = [
-    { title: "Customer Master", icon : <User size={16}/>, path: "/application-form" },
-    { title: "Branch Master", icon : <MapPin size={16}/>, path: "/branch-master" },
-    { title: "Authentication Form", icon: <ClipboardList size={16} />, path: "/viewauthenticationform" },
-    
+    { title: "Customer Master", icon: <User size={16} />, path: "/application-form" },
+    { title: "Branch Master", icon: <MapPin size={16} />, path: "/branch-master" },
+
+
     // { title: "Drop 3", path: "/drop3" },
   ];
   return (
@@ -27,7 +28,7 @@ const Sidebar = () => {
         <div className="flex  w-full items-center justify-start py-2 bg-[#004f9e] border-neutral-300">
           <Link to={"/"} className="w-full">
             <div className="mx-auto flex justify-center items-center w-full  ">
-              <img src="/Logo2.png" alt="" width={100} className="object-cover "/>
+              <img src="/Logo2.png" alt="" width={100} className="object-cover " />
             </div>
           </Link>
         </div>
@@ -39,19 +40,17 @@ const Sidebar = () => {
                 to={items.path}
                 className={({ isActive }) =>
                   `flex font-medium text-sm items-center gap-3 w-full px-3 py-2 group
-                                    ${
-                                      isActive
-                                        ?  "text-white bg-[#06c] hover:bg-[#06c] hover:text-white transition-all duration-300"
-                                        : " text-white hover:bg-[#06c] hover:text-white transition-all duration-300"
-                                    }`
+                                    ${isActive
+                    ? "text-white bg-[#06c] hover:bg-[#06c] hover:text-white transition-all duration-300"
+                    : " text-white hover:bg-[#06c] hover:text-white transition-all duration-300"
+                  }`
                 }
               >
                 {({ isActive }) => (
                   <>
                     <span
-                      className={`transition-transform duration-200 ${
-                        isActive ? "scale-105 " : "group-hover:scale-105"
-                      }`}
+                      className={`transition-transform duration-200 ${isActive ? "scale-105 " : "group-hover:scale-105"
+                        }`}
                     >
                       {items.icon}
                     </span>
@@ -71,9 +70,8 @@ const Sidebar = () => {
                 Masters
                 <ChevronDown
                   size={16}
-                  className={`ml-auto transition-transform duration-200 ${
-                    isMasterOpen ? "rotate-180" : ""
-                  }`}
+                  className={`ml-auto transition-transform duration-200 ${isMasterOpen ? "rotate-180" : ""
+                    }`}
                 />
               </button>
               {isMasterOpen && (
@@ -84,11 +82,10 @@ const Sidebar = () => {
                       to={subItem.path}
                       className={({ isActive }) =>
                         `flex font-medium text-xs items-center gap-2 w-full px-3 py-2 group
-                                          ${
-                                            isActive
-                                              ? " text-white bg-[#06c] hover:bg-[#06c] hover:text-white transition-all duration-300"
-                                              : " text-white hover:bg-[#06c] hover:text-white transition-all duration-300"
-                                          }`
+                                          ${isActive
+                          ? " text-white bg-[#06c] hover:bg-[#06c] hover:text-white transition-all duration-300"
+                          : " text-white hover:bg-[#06c] hover:text-white transition-all duration-300"
+                        }`
                       }
                     >
                       {subItem.icon}{subItem.title}
